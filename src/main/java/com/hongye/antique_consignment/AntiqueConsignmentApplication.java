98aac813db917bf6256f0f1831aff0a662cdc713
RuiChen12 <100800368+RuiChen12@users.noreply.github.com>
2025-06-22 17:47:26 -1000

    Configuration

---
+++ src/main/java/com/hongye/antique_consignment/AntiqueConsignmentApplication.java
@@ -0,0 +1,13 @@
+package com.hongye.antique_consignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntiqueConsignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntiqueConsignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hongye/antique_consignment/AntiqueConsignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hongye.antique_consignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntiqueConsignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
